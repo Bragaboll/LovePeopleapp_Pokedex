084cfc9177665292218b035f5999a56a616025a3
Bragaboll <bragaboll@hotmail.com>
2021-12-26 16:26:36 -0300

    ataulizações

--- lib/view/PokeDetailPage.dart
+++ lib/view/PokeDetailPage.dart
@@ -12,88 +12,94 @@ class PokeDetailPage extends StatelessWidget {
         title: Text(item.name),
       ),
       body: Container(
-        padding: EdgeInsets.all(10.0),
-        child: Center(
-          child: Column(
-            children: <Widget>[
-              Card(
-                margin: EdgeInsets.all(10),
-                child: Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        width: double.maxFinite,
-                        height: 200,
-                        decoration: BoxDecoration(
-                          color: Colors.blue[50],
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: Stack(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: Align(
-                                alignment: Alignment.topRight,
-                                child: Text('#${item.number}'),
-                              ),
+        padding: EdgeInsets.all(5),
+        child: Column(
+          children: <Widget>[
+            Card(
+              margin: EdgeInsets.all(10),
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: double.maxFinite,
+                      height: 200,
+                      decoration: BoxDecoration(
+                        color: Colors.blue[50],
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Stack(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Align(
+                              alignment: Alignment.topRight,
+                              child: Text('#${item.number}'),
                             ),
-                            Center(
-                              child: Image.network(
-                                item.thumbnailImage,
-                              ),
+                          ),
+                          Center(
+                            child: Image.network(
+                              item.thumbnailImage,
                             ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Text(
-                        item.description,
-                        style: TextStyle(fontSize: 14),
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Text(
-                        'Tipo:',
-                        style: TextStyle(
-                          fontWeight: FontWeight.w800,
-                        ),
-                      ),
-                      Text(
-                        item.type
-                            .toString()
-                            .replaceAll('[', '')
-                            .replaceAll(']', '')
-                            .replaceAll(',', ''),
-                        style: TextStyle(fontSize: 14),
-                      ),
-                      Divider(),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Text(
-                        'Fraquezas:',
-                        style: TextStyle(fontWeight: FontWeight.w800),
-                      ),
-                      Text(
-                        item.weakness
-                            .toString()
-                            .replaceAll('[', '')
-                            .replaceAll(']', '')
-                            .replaceAll(',', '')
-                            .replaceAll(',', ''),
-                        style: TextStyle(fontSize: 14),
-                      ),
-                    ],
-                  ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      item.description,
+                      style: TextStyle(fontSize: 13.8),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      'Tipo:',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w800, fontSize: (15)),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      item.type
+                          .toString()
+                          .replaceAll('[', '')
+                          .replaceAll(']', '')
+                          .replaceAll(',', ''),
+                      style: TextStyle(fontSize: 15),
+                    ),
+                    Divider(
+                      color: Colors.grey,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      'Fraquezas:',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w800, fontSize: (15)),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      item.weakness
+                          .toString()
+                          .replaceAll('[', '')
+                          .replaceAll(']', '')
+                          .replaceAll(',', '')
+                          .replaceAll(',', ''),
+                      style: TextStyle(fontSize: 14),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
